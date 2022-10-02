99d925979d72b050477450ddac5b5b922bbe66a0
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-10-02 07:50:10 +0000

    【park】終了

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module PF
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #ActiveStorage上書きしない
+    config.active_storage.replace_on_assign_to_many = false
+    config.time_zone = 'Tokyo'
   end
 end
