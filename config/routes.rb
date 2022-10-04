f44ab87418f0b0c1211b1d5f2248313d3d3fb1e7
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-10-04 16:29:02 +0000

    【admin/customer】実装済

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- 
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
@@ -12,11 +12,12 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :update]
   end
-  
+
   scope module: :public do
     root to: 'homes#top'
     get '/about' => 'homes#about'
