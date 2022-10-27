f9940d3cd19cf457ba633ae7e8d7e9bd3e1d215b
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-10-27 04:50:49 +0000

    【admin】修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,19 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
+
   def top
-    @contacts = Contact.all.page(params[:page]).per(10)
+    contacts = Contact.all
+    if params[:status]
+      if params[:status] == "確認中"
+        @contacts = Contact.where(status: "確認中").page(params[:page]).per(10)
+      elsif params[:status] == "対応中"
+        @contacts = Contact.where(status: "対応中").page(params[:page]).per(10)
+      else
+        @contacts = Contact.where(status: "対応済").page(params[:page]).per(10)
+      end
+    else
+      @contacts = contacts.page(params[:page]).per(10)
+    end
     @badparks = Park.where(latitude: nil).order(created_at: :desc).page(params[:page]).per(8)
   end
 end
