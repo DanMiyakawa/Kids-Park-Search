bb509dba057ca4ebec8a5a61b946ae4fd0775254
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-10-27 02:06:57 +0000

    【admin】編集

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,6 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
     @contacts = Contact.all.page(params[:page]).per(10)
+    @badparks = Park.where(latitude: nil).order(created_at: :desc).page(params[:page]).per(8)
   end
 end
--- app/controllers/admin/parks_controller.rb
+++ app/controllers/admin/parks_controller.rb
@@ -3,7 +3,7 @@ class Admin::ParksController < ApplicationController
 
   def index
     @parks = Park.where.not(latitude: nil).order(created_at: :desc).page(params[:page]).per(8)
-    @butparks = Park.where(latitude: nil).order(created_at: :desc)
+    @badparks = Park.where(latitude: nil).order(created_at: :desc)
   end
 
   def show
