54cf259fe9f46c6eeee0255da253f6b4b4ebb76e
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-11-01 05:43:39 +0000

    【AI】コメント言語処理実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_09_001817) do
+ActiveRecord::Schema.define(version: 2022_11_01_043141) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2022_10_09_001817) do
     t.integer "child_moon_age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "contacts", force: :cascade do |t|
